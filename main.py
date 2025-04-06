ac5c8d7b439d5ba0355beabfd5e8cd6e15d71187
Alisson Zampietro <alisson.zampietro@gmail.com>
2025-04-06 20:26:25 +0100

    feat: adding more informations to dataset

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.title('Main App')
+
+st.set_page_config(
+    page_title="UFO Sightings Dashboard",
+    page_icon="🛸",
+    layout="wide"
+)
+
+# This will automatically discover all pages in the pages directory
+# and create navigation for them
+
+st.title('UFO Sightings Dashboard')
+st.write("Welcome to the UFO Sightings analysis dashboard!")
--- pages/yearly_reports.py
+++ pages/yearly_reports.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 st.title("Yearly Appearances Reports")
 
-@st.cache_data
+total_items_to_show = 10
+
+@st.cache_data()
 def load_data():
-    return pd.read_csv("data/ufo.csv", on_bad_lines='skip')
+    return pd.read_csv("data/ufo.csv", on_bad_lines='skip', low_memory=False)
 
 df = load_data()
 original_df = df
@@ -19,6 +21,6 @@ df = df.groupby("year").size().reset_index(name="count")
 
 st.line_chart(df, x="year", y="count", x_label="Years", y_label="Total Ocurrences")
 st.write(f"""
-    From :green[{min_year:g}] to :green[{max_year:g}] a total of {len(original_df)} reports were registered.
+    From :red[{min_year:g}] to :red[{max_year:g}] a total of :green[{len(original_df)}] reports were registered.
 """)
 st.dataframe(original_df)
